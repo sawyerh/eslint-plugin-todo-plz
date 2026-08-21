222bf0fe6cfefb4af5c45c50a577941fc10ffdd8
Sawyer <git@sawyerh.com>
2026-08-20 20:46:38 -0700

    Match standalone TODO terms

--- tests/ticket-ref.js
+++ tests/ticket-ref.js
@@ -51,6 +51,14 @@ ruleTester.run("ticket-ref", rule, {
       options: [options.jira],
     },
     {
+      code: "// NOTTODO is a documentation label",
+      options: [options.jira],
+    },
+    {
+      code: "// TODO_LIST stores documentation tasks",
+      options: [options.jira],
+    },
+    {
       code: `/**
               * Description
               * TODO (PROJ-123): Connect to the API
